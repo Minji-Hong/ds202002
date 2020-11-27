c446ce5e63b24bbf520e45ee42af5fce345abf5a
Minji-Hong <hmj6741@nate.com>
2020-11-27 20:02:34 +0900

    TermProject2 upload

---
+++ TermProject/TermProject2/src/Main.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+	
+	static int[] data;
+	static int N, a, b;
+	
+	public static int max() { // 최대값 구하는 함수 선언
+		int value=0;
+		for(int i=a-1; i<b; i++) {
+			if(data[i]>value) value=data[i];
+		}
+		return value;
+	}
+	public static int min() { // 최소값 구하는 함수 선언
+		int value=100000;
+		for(int i=a-1; i<b; i++) {
+			if(data[i]<value) value=data[i];
+		}
+		return value;
+	}
+	public static int sum() { // 합계 구하는 함수 선언
+		int value=0;
+		for(int i=a-1; i<b; i++) {
+			value+=data[i];
+		}
+		return value;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		Random rand = new Random();
+		
+		System.out.print("데이터 개수 N과 구간 개수 k입력> ");
+		N = scan.nextInt();
+		int k = scan.nextInt();
+		
+		long t1 = System.currentTimeMillis();
+		
+		data = new int[N];
+		for(int i=0; i<N; i++) { // N개의 데이터를 랜덤함수로 생성
+			data[i]=rand.nextInt(1000);
+		}
+	
+		for(int i=0; i<k; i++) { // k개 구간에 대해 최소, 최대, 합계 구하기
+			b = rand.nextInt(N)+1;
+			a = rand.nextInt(b)+1;
+			System.out.println((i+1)+"구간 ("+a+","+b+") : 최소값 = "+min()+" 최대값 = "+max()+" 합계 = "+sum());
+		}
+		
+		long t2 = System.currentTimeMillis();
+		System.out.println("Elasped time is "+(t2-t1)+"ms.");
+		scan.close();
+	}
+}
